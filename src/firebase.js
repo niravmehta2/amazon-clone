5cbb9611aa515a655f5274d85dc37dfcc9e7096e
niravmehta2 <32747350+niravmehta2@users.noreply.github.com>
2020-09-10 00:19:04 +0200

    Add files via upload

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBtnsIts9WcYFJehzaj5hiFHUWNjM43jP4",
+    authDomain: "challenge-f6338.firebaseapp.com",
+    databaseURL: "https://challenge-f6338.firebaseio.com",
+    projectId: "challenge-f6338",
+    storageBucket: "challenge-f6338.appspot.com",
+    messagingSenderId: "1042313805477",
+    appId: "1:1042313805477:web:f8ca05e1abbe795824574c",
+    measurementId: "G-0699X546ZF"
+  };
